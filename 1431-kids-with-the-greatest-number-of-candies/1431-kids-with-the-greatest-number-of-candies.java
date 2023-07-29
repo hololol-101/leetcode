4c9cafbb417289bd7c62d68019a35242681b4737
Pingu <55730449+hololol-101@users.noreply.github.com>
2023-07-29 15:49:30 +0900

    Time: 1 ms (75.18%), Space: 41.7 MB (35.78%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> answer = new ArrayList<>();
+        int max = 0;
+        for(int c: candies){
+            if(max<c){
+                max = c;    
+            }else{
+                continue;
+            }
+        }
+        
+        for(int c:candies){
+            boolean res = (c+extraCandies>=max) ? true : false;
+            answer.add(res);
+        }
+        
+        return answer;
+        
+    }
+}
