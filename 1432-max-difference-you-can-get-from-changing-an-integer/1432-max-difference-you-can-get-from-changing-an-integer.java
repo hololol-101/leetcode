dea55d4bf307b13c88fdbc760d1d9f4a9f9907ee
Pingu <55730449+hololol-101@users.noreply.github.com>
2023-07-29 16:54:59 +0900

    Time: 8 ms (10.91%), Space: 39.9 MB (29.09%) - LeetHub

--- 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.java
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.java
@@ -1,35 +1,24 @@
 class Solution {
     public int maxDiff(int num) {
         String nstr = Integer.toString(num);
-        Character first = nstr.charAt(0);
-        int max =0;
-        int min =0;
-            
-        if(first=='9'){
-            for(int i=1; i<nstr.length(); i++){
-                if(nstr.charAt(i)-'9'!=0){
-                    max = Integer.parseInt(nstr.replaceAll(""+nstr.charAt(i), "9"));
-                    break;
-                }
-            }
-            if(max==0) max = num;
+        int result = 0;
+        
+        if(nstr.charAt(0)=='9'){
+            String str = nstr.replaceAll("[9]", "");
+            if(str.length()==0) result = num;
+            else result = Integer.parseInt(nstr.replaceAll(""+str.charAt(0), "9"));
         }else{
-            max = Integer.parseInt(nstr.replaceAll(""+first, "9"));
+            result = Integer.parseInt(nstr.replaceAll(""+nstr.charAt(0), "9"));   
+        }
+        if(nstr.charAt(0)=='1'){
+            String str = nstr.replaceAll("[1,0]", "");
+            if(str.length() == 0) result -= num;
+            else result -= Integer.parseInt(nstr.replaceAll(""+str.charAt(0), "0"));
+        }else{
+            result -= Integer.parseInt(nstr.replaceAll(""+nstr.charAt(0), "1"));   
         }
         
-        
-        if(first=='1'){
-            for(int i=1; i<nstr.length(); i++){
-                if(nstr.charAt(i)-'1'>0){
-                    min = Integer.parseInt(nstr.replaceAll(""+nstr.charAt(i), "0"));
-                    break;
-                }
-            }
-            if(min==0) min = num;
-        }else{
-            min = Integer.parseInt(nstr.replaceAll(""+first, "1"));
-        }
-        return max-min;  
+        return result;  
     }
   
 }
