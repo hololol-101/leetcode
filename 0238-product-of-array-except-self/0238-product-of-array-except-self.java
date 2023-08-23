399388194b84a026caf222165a57df13d7b17555
Pingu <55730449+hololol-101@users.noreply.github.com>
2023-08-23 23:58:43 +0900

    Time: 2 ms (58.94%), Space: 54.3 MB (5.47%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int mul = 1;
+        int zeroCnt = 0;
+        for(int i:nums){
+           if(i!=0){
+               mul*=i;
+           } else{
+               zeroCnt++;
+           }
+        }
+        
+        //Zero 수가 2개 이상일 경우 모든 결과값은 0
+        if(zeroCnt>1){
+            return new int[nums.length];
+        }
+        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 0){
+                nums[i] = mul;
+            }else{
+                if(zeroCnt>0)nums[i] = 0;
+                else nums[i] = mul/nums[i];             
+            }
+        }
+        return nums;
+        
+    }
+}
