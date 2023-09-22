3a62e7330a352bf872faaac4a52580087ec6a19e
Pingu <55730449+hololol-101@users.noreply.github.com>
2023-09-22 23:00:16 +0900

    Time: 8 ms (32.67%), Space: 44.3 MB (28.48%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -2,30 +2,24 @@ class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
         for(int i:asteroids){
+            stack.push(i);
             if(i<0){
-                boolean check = true ;
-                while(stack.size()>0 && i<0 && check){
-                    check = false;
+                while(stack.size()>1 && i<0 ){
+                    i = stack.pop();
                     int p = stack.pop();
                     if(p<0){
                         stack.push(p);
                         stack.push(i);
+                        break;
                     }else{
-                        if(Math.abs(i)<Math.abs(p)){
-                            i=p;
-                            check = true;
-                        }else if(Math.abs(i)>Math.abs(p)){
-                            check = true;
+                        if(Math.abs(i)==Math.abs(p)){
+                            break;
                         }else{
-                            check = false;
+                            i = (Math.abs(i)>Math.abs(p))?i:p;
+                            stack.push(i);
                         }
                     }
-                    
                 }
-                if(check) stack.push(i);
-                
-            }else{
-                stack.push(i);
             }
         }
 
