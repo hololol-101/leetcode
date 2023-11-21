33a563c78ff2bcc511baee4933b8551fa3b1217c
Pingu <55730449+hololol-101@users.noreply.github.com>
2023-11-21 21:39:32 +0900

    Time: 20 ms (47.65%), Space: 56.1 MB (23.44%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,23 +1,22 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
         int answer = 0;
-        HashMap<Integer, Integer> hm = new HashMap();
-        for(int i=0; i<nums.length; i++){
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
-        }
-        Iterator<Integer> it =  hm.keySet().iterator();
-        while(it.hasNext()){
-            int n = it.next();
-            if(n*2==k){
-                answer += hm.get(n)/2;
-                hm.put(n, 0);
+        Arrays.sort(nums);
+        int left = 0; 
+        int right = nums.length-1;
+        
+        while(left<right){
+            if(nums[left]+nums[right]==k){
+                left++;
+                right--;
+                answer++;
+            }                
+            if(nums[left]+nums[right]>k){
+                right--;
             }
-            if(hm.getOrDefault(n, 0)*hm.getOrDefault(k-n,0)!=0){   
-                answer += Math.min(hm.get(n), hm.get(k-n));
-                hm.put(n,0);
-                hm.put(k-n,0);
+            if(nums[left]+nums[right]<k){
+                left++;
             }
-
         }
         return answer;
     }
