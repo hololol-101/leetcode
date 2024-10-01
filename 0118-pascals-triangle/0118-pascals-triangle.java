a18ce7dda99c90ff1510cf7edc7d942a92145316
Pingu <55730449+hololol-101@users.noreply.github.com>
2024-10-01 15:38:43 +0900

    Time: 1 ms (84.07%), Space: 41.8 MB (63.00%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -2,8 +2,13 @@ class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> answer = new ArrayList<>();
         for(int i=0; i<numRows; i++){
-            List<Integer> col = new ArrayList<>();
-            
+            List<Integer> col = new ArrayList<>(i+1);
+            answer.add(col);
+        }
+        
+        
+        for(int i=0; i<numRows; i++){            
+            List col = answer.get(i);
             for(int j=0; j<=i; j++){
                 if(j==0 ||j==i){
                     col.add(1);
@@ -11,8 +16,8 @@ class Solution {
                     col.add(answer.get(i-1).get(j-1)+answer.get(i-1).get(j));
                 }
             }
-            answer.add(col);
         }
+        
         return answer;
     }
 }
