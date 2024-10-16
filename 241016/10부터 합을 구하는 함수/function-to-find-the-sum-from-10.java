18d9b8c35dd0a6f06279ae3fca7120346d9d116b
Pingu <55730449+hololol-101@users.noreply.github.com>
2024-10-16 22:29:19 +0900

    Title: 10부터 합을 구하는 함수, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241016/10부터 합을 구하는 함수/function-to-find-the-sum-from-10.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = (n+10)*(n-10+1)/2;
+        System.out.println(answer);
+    }
+}
