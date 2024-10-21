0d6c4b22f9f87592eb0ccacda43a6c7c0044df36
Pingu <55730449+hololol-101@users.noreply.github.com>
2024-10-21 20:01:34 +0900

    Title: 함수 실행 횟수, Time: 3064ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

---
+++ 241021/함수 실행 횟수/number-of-function-executions.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(codetree(n));
+    }
+    static int codetree(int n){
+        if(n<2){
+            return 1;
+        }
+        return codetree(n-1) + codetree(n-2) +1;
+     }
+}
