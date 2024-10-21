641fecc024d767ef1e19ce5e6835b2f18a0e0f85
Pingu <55730449+hololol-101@users.noreply.github.com>
2024-10-21 20:19:16 +0900

    Title: 함수 실행 횟수, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241021/함수 실행 횟수/number-of-function-executions.java
+++ 241021/함수 실행 횟수/number-of-function-executions.java
@@ -4,12 +4,12 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println(codetree(n));
-    }
-    static int codetree(int n){
-        if(n<2){
-            return 1;
+        int[] arr = new int [n];
+        arr[0] = 1;
+        arr[1] = 3;
+        for(int i=2; i<n; i++){
+            arr[i] = arr[i-1]+arr[i-2]+1;
         }
-        return codetree(n-1) + codetree(n-2) +1;
-     }
+        System.out.println(arr[n-1]);
+    }
 }
