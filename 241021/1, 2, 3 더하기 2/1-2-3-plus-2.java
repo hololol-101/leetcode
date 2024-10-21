d8120e7bb2d4244eb8d86802c4fae6828d7a3ff7
Pingu <55730449+hololol-101@users.noreply.github.com>
2024-10-21 21:24:16 +0900

    Title: 1, 2, 3 더하기 2, Time: 106ms, Memory: 12MB, Status: Passed - CodeTree

--- 241021/1, 2, 3 더하기 2/1-2-3-plus-2.java
+++ 241021/1, 2, 3 더하기 2/1-2-3-plus-2.java
@@ -5,32 +5,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int mod = 1000000007;
-        int[][]arr = new int[n+1][4];
-        arr[1][1] = 1;
-        arr[2][2] = 1;
+        int[][]arr = new int[n+1][3];
+        arr[1][0] = 1;
+        arr[2][1] = 1;
+        arr[3][0] = 1;
         arr[3][1] = 1;
         arr[3][2] = 1;
-        arr[3][3] = 1;
         for(int i=4; i<=n; i++){
-            arr[i][1] = (arr[i-1][2] +arr[i-1][3])%mod;
-            arr[i][2] = (arr[i-2][1] +arr[i-2][3])%mod;
-            arr[i][3] = (arr[i-3][1] +arr[i-3][2])%mod;
+            arr[i][0] = (arr[i-1][1] +arr[i-1][2])%mod;
+            arr[i][1] = (arr[i-2][0] +arr[i-2][2])%mod;
+            arr[i][2] = (arr[i-3][0] +arr[i-3][1])%mod;
         }
-        int answer = (arr[n][1]+arr[n][2])%mod ;
-        answer = (answer +arr[n][3])%mod;
+        int answer = (arr[n][0]+arr[n][1])%mod ;
+        answer = (answer +arr[n][2])%mod;
         System.out.println(answer);
     }
 
 }
-//1 = 1 = 1 + 0 + 0
-//2 = 1 = 0 + 1 + 0
-//3 = 0 = 1 + 1 + 1
-//4 = 3 = 2 + 0 + 1
-//5 = 4 = 1 + 2 + 1
-//6 = 8 = 3 + 3 + 2 
-//7 = 9
-//8 = 12
-//9 = 21
-//10 = 27
-//11 = 37
-//12 = 58
